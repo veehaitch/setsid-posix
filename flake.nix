bb57887660a061674280ea18b743c9db79405ea1
Vincent Haupert <mail@vincent-haupert.de>
2021-03-03 08:23:18 +0100

    Nix: init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nmattia/naersk";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk, flake-compat }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        cargoTOML = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        name = cargoTOML.package.name;
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+      in
+      rec {
+        # `nix build`
+        packages.${name} = naersk-lib.buildPackage {
+          pname = name;
+          root = ./.;
+        };
+        defaultPackage = packages.${name};
+
+        # `nix run`
+        apps.${name} = flake-utils.lib.mkApp {
+          drv = packages.${name};
+        };
+        defaultApp = apps.${name};
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          name = "${name}-dev-shell";
+          nativeBuildInputs = [ rust ];
+          shellHook = ''
+            export PATH=$PWD/target/debug:$PATH
+          '';
+        };
+      });
+}
