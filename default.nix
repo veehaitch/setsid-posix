1911a46168051d8321314a1532f630044adac1df
Vincent Haupert <mail@vincent-haupert.de>
2022-01-24 14:45:25 +0100

    Remove naersk, update deps

---
+++ default.nix
@@ -0,0 +1,73 @@
+{ rustPlatform
+, lib
+, stdenv
+, libiconv
+, pkg-config
+, doCodeStyleCheck ? true
+}:
+let
+  cargoTOML = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  # Filter out VCS files and files unrelated to the Rust package
+  filterRustSource = src: with lib; cleanSourceWith {
+    filter = cleanSourceFilter;
+    src = cleanSourceWith {
+      inherit src;
+      filter = name: type:
+        let pathWithoutPrefix = removePrefix (toString src) name; in
+          ! (
+            hasPrefix "/.github" pathWithoutPrefix ||
+            pathWithoutPrefix == "/.gitignore" ||
+            pathWithoutPrefix == "/LICENSE" ||
+            pathWithoutPrefix == "/README.md" ||
+            pathWithoutPrefix == "/flake.lock" ||
+            pathWithoutPrefix == "/flake.nix"
+          );
+    };
+  };
+in
+rustPlatform.buildRustPackage rec {
+  pname = cargoTOML.package.name;
+  version = cargoTOML.package.version;
+  src = filterRustSource ./.;
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  inherit doCodeStyleCheck;
+
+  preBuildPhases = lib.optionals doCodeStyleCheck [
+    "codeStyleConformanceCheck"
+  ];
+
+  codeStyleConformanceCheck = ''
+    header "Checking Rust code formatting"
+    cargo fmt -- --check
+
+    header "Running clippy"
+    # clippy - use same checkType as check-phase to avoid double building
+    if [ "''${cargoCheckType}" != "debug" ]; then
+        cargoCheckProfileFlag="--''${cargoCheckType}"
+    fi
+    argstr="''${cargoCheckProfileFlag} --workspace --all-features --tests "
+    cargo clippy -j $NIX_BUILD_CORES \
+       $argstr -- \
+       -D clippy::pedantic \
+       -D warnings
+  '';
+
+  # build dependencies
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  # runtime dependencies
+  buildInputs = lib.optionals stdenv.isDarwin [
+    libiconv
+  ];
+
+  doCheck = true;
+
+  postInstall = ''
+    # Provide a symlink from `setsid-posix` to `setsid` for compat
+    ln -sr "$out/bin/${cargoTOML.package.name}" "$out/bin/setsid"
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -1,152 +1,141 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     let
       cargoTOML = builtins.fromTOML (builtins.readFile ./Cargo.toml);
       name = cargoTOML.package.name;
       version = cargoTOML.package.version;
-    in
-    flake-utils.lib.eachDefaultSystem
-      (system:
+
+      lib = nixpkgs.lib;
+
+      rust-toolchainOverlay = final: prev: {
+        rust-toolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+      };
+
+      recursiveMerge = with lib; foldl recursiveUpdate { };
+      eachSystem = systems: f: flake-utils.lib.eachSystem systems (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ rust-overlay.overlay ];
-          };
-
-          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-
-          naersk-lib = naersk.lib."${system}".override {
-            cargo = rust;
-            rustc = rust;
+            overlays = [ rust-overlay.overlay rust-toolchainOverlay self.overlay ];
           };
         in
-        rec {
-          # `nix build`
-          packages.${name} = naersk-lib.buildPackage {
-            pname = name;
-            root = ./.;
-
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-            ];
-
-            buildInputs = with pkgs; lib.optionals stdenv.isDarwin [
-              libiconv
-            ];
-
-            doCheck = true;
-            cargoTestCommands = x: x ++ [
-              # clippy
-              ''cargo clippy --all --all-features --tests -- -D clippy::pedantic -D warnings''
-              # rustfmt
-              ''cargo fmt -- --check''
-            ];
-
-            overrideMain = _: {
-              postInstall = ''
-                # Provide a symlink from `setsid-posix` to `setsid` for compat
-                ln -sr "$out/bin/${name}" "$out/bin/setsid"
-              '';
-            };
-          };
-          defaultPackage = packages.${name};
-
-          # `nix run`
-          apps.${name} = flake-utils.lib.mkApp {
-            drv = packages.${name};
-          };
-          defaultApp = apps.${name};
-
-          # `nix check`
-          checks.test-util-linux-overlay =
-            let
-              expectedVersion =
-                if pkgs.stdenv.isLinux
-                then "setsid from util-linux ${pkgs.util-linux.version}"
-                else "${name} ${version}";
-              testPkgs = import nixpkgs {
-                inherit system;
-                overlays = [ self.overlay ];
-              };
-            in
-            with testPkgs; runCommand "test-utillinux-overlay"
-              {
-                # Uses `utillinux` instead of `util-linux` to make sure the alias works
-                buildInputs = [ utillinux tree ];
-              } ''
-              set -euo pipefail
-
-              VERSION=$(setsid -V)
-              if [[ $? != 0 ]]; then
-                echo "Executing setsid failed"
-              elif [[ $VERSION == "${expectedVersion}" ]]; then
-                echo "Found expected version: $VERSION"
-                echo "Directory tree:"
-                tree ${utillinux}
-                mkdir $out
-              else
-                echo "Didn't find expected setsid from util-linux: $VERSION"
-              fi
-            '';
-
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            name = "${name}-dev-shell";
-
-            nativeBuildInputs = [ rust ] ++ (with pkgs; [ pkg-config rust-analyzer ]);
-            buildInputs = with pkgs; lib.optionals stdenv.isDarwin [
-              libiconv
-            ];
-
-            RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
-
-            shellHook = ''
-              export PATH=$PWD/target/debug:$PATH
-            '';
-          };
-        }) // {
-      overlay = final: prev:
-        let
-          setsid = self.packages.${prev.system}.${name};
-          util-linux-setsid = prev.runCommandNoCC "util-linux-setsid"
-            {
-              propagatedBuildInputs = [ prev.util-linux ];
-            } ''
-            mkdir "$out"
-            ln -s ${prev.util-linux}/* "$out/"
-
-            rm -f     "$out/bin"
-            mkdir     "$out/bin"
-            chmod 755 "$out/bin"
-            ln -s ${prev.util-linux}/bin/* "$out/bin/"
-
-            ln -s "${setsid}/bin/${name}" "$out/bin/setsid"
-          '';
-          util-linux =
-            if prev.stdenv.isLinux
-            then prev.util-linux
-            else util-linux-setsid;
-        in
-        {
-          "${name}" = setsid;
-          inherit util-linux;
+        f pkgs
+      );
+      eachDefaultSystem = eachSystem flake-utils.lib.defaultSystems;
+    in
+    recursiveMerge [
+      #
+      # COMMON OUTPUTS FOR ALL SYSTEMS
+      #
+      (eachDefaultSystem (pkgs: rec {
+        # `nix build`
+        packages.${name} = pkgs.callPackage ./default.nix {
+          doCodeStyleCheck = false;
         };
-    };
+        defaultPackage = packages.${name};
+
+        # `nix check`
+        checks."${name}-codestyle" = pkgs.callPackage ./default.nix {
+          doCodeStyleCheck = true;
+          rustPlatform = with pkgs; makeRustPlatform {
+            cargo = rust-toolchain;
+            rustc = rust-toolchain;
+          };
+        };
+
+        checks.nixpkgs-fmt = pkgs.runCommand "check-nix-format" { } ''
+          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+          mkdir $out #sucess
+        '';
+
+        checks.test-util-linux-overlay =
+          let
+            expectedVersion =
+              if pkgs.stdenv.isLinux
+              then "setsid from util-linux ${pkgs.util-linux.version}"
+              else "${name} ${version}";
+            testPkgs = import nixpkgs {
+              inherit (pkgs) system;
+              overlays = [ self.overlay ];
+            };
+          in
+          with testPkgs; runCommand "test-utillinux-overlay"
+            {
+              # Uses `utillinux` instead of `util-linux` to make sure the alias works
+              buildInputs = [ utillinux tree ];
+            } ''
+            set -euo pipefail
+
+            VERSION=$(setsid -V)
+            if [[ $? != 0 ]]; then
+              echo "Executing setsid failed"
+            elif [[ $VERSION == "${expectedVersion}" ]]; then
+              echo "Found expected version: $VERSION"
+              echo "Directory tree:"
+              tree ${utillinux}
+              mkdir $out
+            else
+              echo "Didn't find expected setsid from util-linux: $VERSION"
+            fi
+          '';
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          name = "${name}-dev-shell";
+
+          nativeBuildInputs = with pkgs; [ pkg-config rust-toolchain rust-analyzer ];
+          buildInputs = with pkgs; lib.optionals stdenv.isDarwin [
+            libiconv
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rust-toolchain}/lib/rustlib/src/rust/library";
+
+          shellHook = ''
+            export PATH=$PWD/target/debug:$PATH
+          '';
+        };
+      }))
+      #
+      # SYSTEM-INDEPENDENT OUTPUTS
+      #
+      {
+        # Overlay to add ragenix and replace agenix
+        overlay = final: prev:
+          let
+            setsid = self.packages.${prev.system}.${name};
+            util-linux-setsid = prev.runCommand "util-linux-setsid"
+              {
+                propagatedBuildInputs = [ prev.util-linux ];
+              } ''
+              mkdir "$out"
+              ln -s ${prev.util-linux}/* "$out/"
+
+              rm -f     "$out/bin"
+              mkdir     "$out/bin"
+              chmod 755 "$out/bin"
+              ln -s ${prev.util-linux}/bin/* "$out/bin/"
+
+              ln -s "${setsid}/bin/${name}" "$out/bin/setsid"
+            '';
+            util-linux =
+              if prev.stdenv.isLinux
+              then prev.util-linux
+              else util-linux-setsid;
+          in
+          {
+            "${name}" = setsid;
+            inherit util-linux;
+          };
+      }
+    ];
 }
