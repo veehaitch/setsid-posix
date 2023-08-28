8592a588f8fadfa60558b18f6b7476847c2f15e1
Vincent Haupert <mail@vincent-haupert.de>
2023-08-28 18:10:36 +0200

    Use clap derive

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 use std::result::Result::{Err, Ok};
 use std::string::String;
 
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg};
+use clap::Parser;
 use nix::unistd::{getpgrp, getpid, setsid, ForkResult};
 use nix::{
     libc::{c_int, wait, EXIT_FAILURE, EXIT_SUCCESS, WEXITSTATUS, WIFEXITED},
@@ -55,78 +55,36 @@ fn create_new_session() {
     }
 }
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 struct Opts {
+    #[arg(
+        short,
+        long,
+        help = "Set the controlling terminal to the current one. Currently not implemented."
+    )]
+    ctty: bool,
+    #[arg(short, long, help = "Always create a new process")]
     fork: bool,
-    program: String,
-    arguments: Vec<String>,
+    #[arg(
+        short,
+        long,
+        help = "Wait for the execution of the program to end, and return the exit value of this program as the return value of setsid"
+    )]
     wait: bool,
-}
-
-fn parse_args() -> Opts {
-    let matches = App::new(crate_name!())
-        .setting(AppSettings::TrailingVarArg)
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(
-            Arg::new("ctty")
-                .help(
-                    "Set the controlling terminal to the current one. \
-                    Currently not implemented.",
-                )
-                .long("ctty")
-                .short('c')
-                .takes_value(false),
-        )
-        .arg(
-            Arg::new("fork")
-                .help("Always create a new process")
-                .long("fork")
-                .short('f')
-                .takes_value(false),
-        )
-        .arg(
-            Arg::new("wait")
-                .help(
-                    "Wait for the execution of the program to end, and return the exit value \
-                    of this program as the return value of setsid",
-                )
-                .long("wait")
-                .short('w')
-                .takes_value(false),
-        )
-        .arg(
-            Arg::new("program")
-                .required(true)
-                .help("The program to run in a new session"),
-        )
-        .arg(
-            Arg::new("arguments")
-                .help("The arguments to pass to `program`, if any")
-                .multiple_values(true)
-                .required(false),
-        )
-        .get_matches();
-
-    if matches.is_present("ctty") {
-        eprintln!("The --ctty flag is currently not implemented");
-        process::exit(EXIT_FAILURE);
-    }
-
-    Opts {
-        fork: matches.is_present("fork"),
-        program: matches.value_of("program").unwrap().to_string(),
-        arguments: matches
-            .values_of("arguments")
-            .unwrap_or_default()
-            .map(std::string::ToString::to_string)
-            .collect(),
-        wait: matches.is_present("wait"),
-    }
+    #[arg(help = "The program to run in a new session")]
+    program: String,
+    #[arg(help = "The arguments to pass to `program`, if any")]
+    arguments: Vec<String>,
 }
 
 fn main() {
-    let opts = parse_args();
+    let opts = Opts::parse();
+
+    if opts.ctty {
+        eprintln!("The --ctty flag is currently not implemented");
+        process::exit(EXIT_FAILURE);
+    }
 
     if opts.fork || is_process_group_leader() {
         fork(opts.wait);
